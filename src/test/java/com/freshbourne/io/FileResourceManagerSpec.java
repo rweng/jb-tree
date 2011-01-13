dafeb5b8e9895b44e491bd6e072b0f0364854930
Robin Wenglewski <robin@wenglewski.de>
2011-01-13 09:16:52 +0100

    clean up spec

--- src/test/java/com/freshbourne/io/FileResourceManagerSpec.java
+++ src/test/java/com/freshbourne/io/FileResourceManagerSpec.java
@@ -7,30 +7,22 @@
  */
 package com.freshbourne.io;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.freshbourne.io.FileResourceManager;
-import com.freshbourne.io.ResourceManager;
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
 
-import  static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class FileResourceManagerSpec {
 	
 	private ResourceManager rm;
-	private final File file;
+	private final File file = new File("/tmp/frm_test");
 	private HashPage page;
-	
-	public FileResourceManagerSpec(){
-		super();
-		System.out.println("creating FileResourceManagerSpec");
-		file = new File("/tmp/frm_test");
-	}
 	
 	@Before
 	public void setUp() throws IOException {
@@ -52,8 +44,8 @@ public class FileResourceManagerSpec {
 	
 	@Test
 	public void shouldBeEmptyAtFirst() throws IOException{
-		assertTrue(rm instanceof ResourceManager);
-		assertEquals(PageSize.DEFAULT_PAGE_SIZE,rm.pageSize());
+		assertTrue(rm != null);
+		assertEquals(PageSize.DEFAULT_PAGE_SIZE, rm.pageSize());
 		assertEquals(0, file.length());
 	}
 	
@@ -66,49 +58,5 @@ public class FileResourceManagerSpec {
 	@Test(expected= ElementNotFoundException.class)
 	public void shouldThrowExceptionIfPageToWriteDoesNotExist() throws IOException{
 		rm.writePage(page);
-	}
-	
-		
-//		// test the page
-//		assertFalse(p.valid());
-//		p.initialize();
-//		assertTrue(p.valid());
-//				
-//		// test saving the page
-//		p.save();
-//		assertEquals(PageSize.DEFAULT_PAGE_SIZE, file.length());
-//		
-//		rm.close();
-//		rm.open();
-//		p = rm.readPage(1);
-//		assertTrue(p.valid());
-//		assertEquals(PageSize.DEFAULT_PAGE_SIZE, p.buffer().capacity());
-//		assertEquals(PageSize.DEFAULT_PAGE_SIZE - 4, p.body().capacity());
-//		
-//		// altering the page size should throw an error
-//		rm.close();
-//		rm = new FileResourceManager(file, PageSize.DEFAULT_PAGE_SIZE * 2);
-//		try{
-//			rm.open();
-//			fail("opening a file with wrong pagesize should throw an error");
-//		} catch (Exception e) {
-//		}
-	
-	
-	public void testByteBuffer(){
-		ByteBuffer b = ByteBuffer.allocate(100);
-		int pos = 10;
-		b.position(pos);
-		b.mark();
-		b.position(100);
-		b.reset();
-		assertEquals(pos, b.position());
-		
-		b.slice();
-		assertEquals(pos, b.position());
-		b.reset();
-		assertEquals(pos, b.position());
-		
-		
 	}
 }
