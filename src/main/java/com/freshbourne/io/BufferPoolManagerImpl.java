ea50cb4789670e9bd4ba6dfaf2ff2d7cfb9c7a8c
Robin Wenglewski <robin@wenglewski.de>
2011-01-29 07:23:16 +0100

    remove duplicate copyright

--- src/main/java/com/freshbourne/io/BufferPoolManagerImpl.java
+++ src/main/java/com/freshbourne/io/BufferPoolManagerImpl.java
@@ -6,13 +6,6 @@
  * For alternative conditions contact the author.
  */
 
-/**
- * This work is licensed under a Creative Commons Attribution-NonCommercial 3.0 Unported License:
- * http://creativecommons.org/licenses/by-nc/3.0/
- * For alternative conditions contact the author.
- * 
- * (c) 2010 "Robin Wenglewski <robin@wenglewski.de>"
- */
 package com.freshbourne.io;
 
 import com.google.inject.Inject;
