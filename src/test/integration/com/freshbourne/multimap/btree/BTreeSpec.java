562cfa8b2cd71b625e8e3bdce28ee1e7c20e4d3c
Robin Wenglewski <robin@wenglewski.de>
2011-10-01 12:41:28 +0200

    refactoring: move performance tests to normal tests

--- src/test/integration/com/freshbourne/multimap/btree/BTreeSpec.java
+++ src/test/integration/com/freshbourne/multimap/btree/BTreeSpec.java
@@ -12,6 +12,7 @@ import static org.junit.Assert.*;
 import java.io.File;
 import java.io.IOException;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.freshbourne.multimap.MultiMap;
@@ -57,5 +58,50 @@ public class BTreeSpec extends MultiMapSpec<Integer, String> {
 		assertEquals(value2, tree.get(key2).get(0));
 	}
 
+	@Test
+	public void shouldWorkWithMassiveValues(){
+		int size = 10000;
+
+		fill(size);
+		
+		assertEquals(size, getMultiMap().getNumberOfEntries());
+		key1 = getProvider().createMaxKey();
+		simpleTests();
+		key1 = getProvider().createMinKey();
+		simpleTests();
+	}
+	
+	@Test @Ignore("does not work yet")
+	public void shouldNotHaveTooMuchOverhead(){
+		int key = getProvider().createRandomKey();
+		String val = getProvider().createRandomValue();
+		
+		int sizeForKey = Integer.SIZE / 8;
+		int sizeForVal = val.length();
+		
+		// insert 10.000 K/V pairs
+		int size = 100000;
+		long start = System.currentTimeMillis();
+		for(int i = 0; i<size;i++){
+			getMultiMap().add(key, val);
+		}
+		
+		getTree().sync();
+		long end = System.currentTimeMillis();
+		
+		File file = new File(path);
+		Long sizeOfData = (long)(size * (sizeForKey + sizeForVal));
+		float realSizePercent = file.length() / sizeOfData * 100; 
+		
+		System.out.println("====== BTREE: SIZE OVERHEAD TEST ======");
+		System.out.println("key + value data inserted:" + sizeOfData / 1024 + "k");
+		System.out.println("fileSize: " + file.length()/1024 + "k ("+realSizePercent+"%)");
+		System.out.println("time for insert w/ sync in millis: " + (end - start));
+		//assertThat("current Size: " + realSizePercent + "%", realSizePercent, lessThan(1000f));
+	}
+	
+	private BTree<Integer, String> getTree(){
+		return (BTree<Integer, String>) getMultiMap();
+	}	
 	
 }
