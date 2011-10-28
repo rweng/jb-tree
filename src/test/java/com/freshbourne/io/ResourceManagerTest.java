c2fefd55cfacb400c7492e15c913a0fa3f8a3878
Robin Wenglewski <robin@wenglewski.de>
2011-10-28 05:43:16 +0200

    fest matchers

--- src/test/java/com/freshbourne/io/ResourceManagerTest.java
+++ src/test/java/com/freshbourne/io/ResourceManagerTest.java
@@ -10,18 +10,19 @@
 
 package com.freshbourne.io;
 
+import org.apache.log4j.Logger;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.security.SecureRandom;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertNotNull;
-import static org.testng.Assert.assertTrue;
+import static org.fest.assertions.Assertions.assertThat;
 
 public class ResourceManagerTest {
 	private ResourceManager rm;
+	private static Logger LOG = Logger.getLogger(ResourceManagerTest.class);
 
 	ResourceManagerTest(ResourceManager rm){
 		checkNotNull(rm);
@@ -36,9 +37,35 @@ public class ResourceManagerTest {
 
 	@Test
 	public void shouldBeEmptyAtFirst() throws IOException {
-		assertTrue(rm != null);
-		assertEquals(PageSize.DEFAULT_PAGE_SIZE, rm.getPageSize());
-		assertEquals(0, rm.numberOfPages()); // 0 pages
+		assertThat(rm.numberOfPages()).isZero();
+		assertThat(rm.getPageSize()).isEqualTo(PageSize.DEFAULT_PAGE_SIZE);
+	}
+
+
+	@Test(groups = "bla")
+	public void performance(){
+		int count = 100000;
+		RawPage[] pages = new RawPage[count];
+
+		SecureRandom srand = new SecureRandom();
+
+		long createStart = System.currentTimeMillis();
+		// create pages
+		for(int i = 0;i<count;i++){
+			pages[i] = rm.createPage();
+		}
+		long createEnd = System.currentTimeMillis();
+		LOG.info("Time for creating "+count+" pages (in ms): " + (createEnd - createStart));
+
+		// randomly write to pages
+		long writeStart = System.currentTimeMillis();
+		for(int i=0; i < count; i++){
+			int page = srand.nextInt() % count;
+			pages[page].bufferForWriting(0).putInt(i);
+			rm.writePage(pages[page]);
+		}
+		long writeEnd = System.currentTimeMillis();
+		LOG.info("Time for writing randomly "+count+" pages (in ms): " + (writeEnd - writeStart));
 	}
 
 }
