0a9dbea7b238339ecaa742d79cf59950ef524368
Robin Wenglewski <robin@wenglewski.de>
2011-10-29 05:30:35 +0200

    adding a cache to AbstractPageManager, again, and and explaination why we need it

--- src/main/java/com/freshbourne/io/AbstractPageManager.java
+++ src/main/java/com/freshbourne/io/AbstractPageManager.java
@@ -9,16 +9,28 @@
  */
 package com.freshbourne.io;
 
+import com.google.common.cache.CacheBuilder;
 import com.google.common.collect.MapMaker;
 import org.apache.log4j.Logger;
 
 import java.io.IOException;
 import java.util.Map;
 
+/**
+ * There is this special use-case when the same page is fetched twice from the PageManager. If we have no cache here,
+ * at least the wrapping LeafNode (not the RawPage) will be a separate instance. If we then change one instance the
+ * change isn't reflected in the second instance.
+ * <p/>
+ * There are basically two ways to avoid that: don't cache any values in the leafPage and always get the values
+ * directly from the RawPage. Or we could cache here, too, so that both times the same instance is returned. Last
+ * option seems better regarding performance.
+ */
 public abstract class AbstractPageManager<T extends ComplexPage> implements PageManager<T> {
 
 	private static final Logger LOG = Logger.getLogger(AbstractPageManager.class);
 	private final PageManager<RawPage> rpm;
+
+	private final Map<Integer, T> cache = new MapMaker().weakValues().makeMap();
 
 	protected AbstractPageManager(PageManager<RawPage> rpm) {
 		this.rpm = rpm;
@@ -39,10 +51,20 @@ public abstract class AbstractPageManager<T extends ComplexPage> implements Page
 	public T getPage(int id) {
 		T result;
 
-		result = createObjectPage(rpm.getPage(id));
+		RawPage page = rpm.getPage(id);
+		if(cache.containsKey(id)){
+			T t = cache.get(id);
+			if(page == t.rawPage())
+				return t;
+			else
+				cache.remove(id);
+		}
+		
+		result = createObjectPage(page);
 
 		try {
 			result.load();
+			cache.put(result.rawPage().id(), result);
 		} catch (IOException e) {
 			// if the page cannot be loaded, something is off.
 			// we should only be able to fetch initialized pages from the rpm.
@@ -72,6 +94,8 @@ public abstract class AbstractPageManager<T extends ComplexPage> implements Page
 
 		if (LOG.isDebugEnabled())
 			LOG.debug("node created: type: \t" + l.getClass().getSimpleName().toString() + "\tid: " + l.rawPage().id());
+
+		cache.put(l.rawPage().id(), l);
 		return l;
 	}
 
@@ -81,8 +105,8 @@ public abstract class AbstractPageManager<T extends ComplexPage> implements Page
 		 */
 	@Override
 	public void removePage(int id) {
+		cache.remove(id);
 		rpm.removePage(id);
-
 	}
 
 	/**
